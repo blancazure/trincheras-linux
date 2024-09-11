c2752c08009de8f74c82a6abda0ce803bfa7c925
blancazure <jblanco@blancazure.com>
2024-09-11 16:43:57 +0200

    Añadido script creación usuarios y grupos

---
+++ capitulo02/scripts/crear-usuarios-grupos.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Declaramos los grupos
+grupos=("desarrollo" "implantacion" "soporte")
+
+# Usuarios asignados a cada grupo
+usuarios_desarrollo=("jblanco" "amartinez" "ilopez" "dalvarez" "icuesta")
+usuarios_implantacion=("aperez" "scernadas")
+usuarios_soporte=("ebarbosa" "jirago")
+
+# Crear los grupos de usuarios
+for grupo in "${grupos[@]}"
+do
+    # Comando para crear el grupo (usar sudo si es necesario)
+    sudo groupadd "$grupo"
+    echo "Grupo creado: $grupo"
+done
+
+# Asignar los usuarios al grupo desarrollo
+for usuario in "${usuarios_desarrollo[@]}"
+do
+    # Crear el usuario y asignarlo al grupo desarrollo
+    sudo useradd -G desarrollo "$usuario"
+    echo "Usuario creado: $usuario y asignado al grupo desarrollo"
+done
+
+# Asignar los usuarios al grupo implantacion
+for usuario in "${usuarios_implantacion[@]}"
+do
+    # Crear el usuario y asignarlo al grupo implantacion
+    sudo useradd -G implantacion "$usuario"
+    echo "Usuario creado: $usuario y asignado al grupo implantacion"
+done
+
+# Asignar los usuarios al grupo soporte
+for usuario in "${usuarios_soporte[@]}"
+do
+    # Crear el usuario y asignarlo al grupo soporte
+    sudo useradd -G soporte "$usuario"
+    echo "Usuario creado: $usuario y asignado al grupo soporte"
+done
